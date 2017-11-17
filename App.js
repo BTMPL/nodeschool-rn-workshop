50feb9eba6be1ecb12a9280157ab262750f327c4
Bartosz Szczecinski <btm@anfo.pl>
2017-11-17 22:16:43 +0100

    01login

--- App.js
+++ App.js
@@ -3,38 +3,11 @@ import {
   StyleSheet, 
   Text, 
   View,
-  FlatList,
   TextInput,
-  KeyboardAvoidingView,
   TouchableOpacity 
 } from 'react-native';
 
-import { StackNavigator } from 'react-navigation';
-
-
-import throttle from "lodash.throttle";
-import uuid from "uuid/v4";
-
-import io from "socket.io-client";
-console.ignoredYellowBox = [
-  'Setting a timer'
-];
-
 const Styles = StyleSheet.create({
-  Message: {
-    padding: 10,
-    backgroundColor: '#ffffff',
-    borderRadius: 5,
-    marginBottom: 10,
-    maxWidth: '95%'
-  },
-  Typing: {
-    padding: 10, 
-    marginBottom: 10, 
-    justifyContent: 'center', 
-    flex: 1, 
-    flexDirection: 'row'
-  },
   Login: {
     flex: 1, 
     flexDirection: 'row', 
@@ -50,152 +23,6 @@ const Styles = StyleSheet.create({
 });
 
 
-
-const Message = ({item, self}) => {
-  const left = self ? '5%' : 0;
-  return (
-    <View style={[Styles.Message, {marginLeft: left}]}>
-      <Text style={{fontWeight: 'bold', color: '#8cdba4'}}>{item.author}</Text>
-      <Text>{item.text}</Text>      
-    </View>
-  )
-}
-
-class Typing extends React.Component {
-  render() {
-    return (
-      <View style={Styles.Typing}>
-        <Text>Ktoś właśnie pisze ...</Text>
-      </View>
-    )
-  }
-}
-
-class ChatScreen extends React.Component {
-
-  keyExtractor = (item) => item.id;
-  typingTimeout = 0;
-
-  constructor(props) {
-    super(props);
-    console.log(props);
-
-    this.state = {
-      userId: uuid(),
-      userName: this.props.navigation.state.params.user,
-      text: '',
-      typing: false,
-      items: []      
-    }        
-  
-  }
-
-  componentDidMount() {
-    this.socket = io('http://192.168.1.27:8080', {jsonp: false});
-
-    this.socket.on('connect', () => {                                                                            
-      console.log('connected');
-    });
-
-    this.socket.on('echo', (data) => {
-      this.setState({
-        items: [
-          ...this.state.items,
-          data
-        ],
-        typing: false
-      }, () => this.list.scrollToEnd());
-    });
-
-    this.socket.on('typing', () => {
-      this.setState({
-        typing: true
-      });
-
-      clearTimeout(this.typingTimeout);
-      this.typingTimeout = setTimeout(() => {
-        this.setState({
-          typing: false
-        })
-      }, 5000)
-    })
-  }
-
-  componentWillUnmount() {
-    this.socket.close();
-  }
-
-  setRef = (el) => this.list = el;
-
-  handleTextChange = (text) => {
-    this.setState({
-      text
-    }, () => {
-      this.handleTyping();
-    })
-  }  
-
-  handleSubmit = () => {
-    if(this.state.text) {
-      const payload = {
-        author: this.state.userName,
-        user: this.state.userId,
-        text: this.state.text
-      };
-
-      this.socket.emit('msg', payload);
-
-      this.setState({
-        text: ''
-      });
-    }
-  }
-
-  handleTyping = throttle(() => {
-    console.log('send typing');
-    this.socket.emit('typing'); 
-  }, 3000, { trailing: false });
-
-  getItems = () => {
-    const items = [
-      ...this.state.items,       
-    ];
-    if(this.state.typing) items.push({ type: 'special', 'id': 'typing'});
-
-    return items;
-  }
-
-  renderItem = ({item}) => {
-    if(typeof item.type === 'undefined') return <Message item={item} self={item.user === this.state.userId} />;
-    else return <Typing />
-  }
-
-  render() {
-    return (
-      <View style={{backgroundColor: '#e6e2df', flex: 1}}>
-        <FlatList
-          ref={this.setRef}
-          style={{padding: 10, flex: 1}}          
-          data={this.getItems()}
-          renderItem={this.renderItem}
-          keyExtractor={this.keyExtractor}
-        />
-        <KeyboardAvoidingView behavior={'padding'} style={{backgroundColor: '#f5f1ee'}}>
-          <View style={{backgroundColor: 'white', padding: 5, margin: 10}}>
-            <TextInput 
-              value={this.state.text}
-              underlineColorAndroid={'transparent'} 
-              autoGrow={true}
-              multiline={true}
-              onSubmitEditing={this.handleSubmit}
-              onChangeText={this.handleTextChange} />
-          </View>
-        </KeyboardAvoidingView>        
-      </View>
-    );
-  }
-}
-
 class HomeScreen extends React.Component {
 
   state = {
@@ -210,7 +37,7 @@ class HomeScreen extends React.Component {
 
   handleSubmit = () => {
     if(this.state.name.length >= 3) {
-      this.props.navigation.navigate('Chat', { user: this.state.name })
+      alert(`Witaj ${this.state.name}`);
     }
   }
 
@@ -241,19 +68,4 @@ class HomeScreen extends React.Component {
   }
 }
 
-const RootNavigator = StackNavigator({
-  Home: {
-    screen: HomeScreen,
-    navigationOptions: () => ({
-      header: null
-    })
-  },
-  Chat: {
-    screen: ChatScreen,
-    navigationOptions: () => ({
-      header: null
-    })    
-  }
-});
-
-export default RootNavigator;
+export default HomeScreen;
