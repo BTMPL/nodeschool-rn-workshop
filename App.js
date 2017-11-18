21bdc87a8866847595bd39acc1fb99b452df3928
Bartosz Szczecinski <btm@anfo.pl>
2017-11-18 07:09:38 +0100

    autocorrect & friends

--- App.js
+++ App.js
@@ -51,6 +51,7 @@ class HomeScreen extends React.Component {
           </View>
           <View style={{width: 150, marginLeft: 10}}>
             <TextInput 
+              autoCorrect={false}
               value={this.state.name} 
               underlineColorAndroid={'white'} 
               style={{color: 'white'}} 
