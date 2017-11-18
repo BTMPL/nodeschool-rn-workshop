e210ee75b2a8f50cb5eedb28bab6ff7f9d4effc6
Bartosz Szczecinski <btm@anfo.pl>
2017-11-18 07:11:24 +0100

    autocorrect & friends

--- App.js
+++ App.js
@@ -122,8 +122,7 @@ class ChatScreen extends React.Component {
               <TextInput 
                 value={this.state.text}
                 underlineColorAndroid={'transparent'} 
-                autoGrow={true}
-                multiline={true}
+                autoCorrect={false}
                 onSubmitEditing={this.handleSubmit}
                 onChangeText={this.handleTextChange} />
             </View>  
@@ -162,6 +161,7 @@ class HomeScreen extends React.Component {
           </View>
           <View style={{width: 150, marginLeft: 10}}>
             <TextInput 
+              autoCorrect={false}
               value={this.state.name} 
               underlineColorAndroid={'white'} 
               style={{color: 'white'}} 
